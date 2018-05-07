3f7255ef194f942543ef2e153deb5075c2e3f753
JustinCB <JustinCB@opmbx.org>
2018-05-07 15:56:09 -0400

    daggon preprocessor

---
+++ SheepShaver/src/kpx_cpu/src/cpu/ppc/ppc_wrap.cc
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include "sysdeps.h"
+#include "ppc_wrap.h"
+extern "C" {
+regpointer c_registers;
+}
+regpointer make_regptr(struct powerpc_registers regs)
+{
+  regpointer ret;
+  ret.gpr = &(regs.gpr[0]);
+  ret.fpr = &(regs.fpr[0]);
+  ret.vr = &(regs.vr[0]);
+  ret.cr = &(regs.cr.cr);
+  ret.xer.so = &(regs.xer.so);
+  ret.xer.ov = &(regs.xer.ov);
+  ret.xer.ca = &(regs.xer.ca);
+  ret.xer.byte_count = &(regs.xer.byte_count);
+  ret.vscr = &(regs.vscr.vscr);
+  ret.vrsave = &(regs.vrsave);
+  ret.fpscr = &(regs.fpscr);
+  ret.lr = &(regs.lr);
+  ret.ctr = &(regs.ctr);
+  ret.pc = &(regs.pc);
+  return ret;
+}
+void init_c_registers(struct powerpc_registers regs)
+{
+    c_registers = make_regptr(regs);
+}
