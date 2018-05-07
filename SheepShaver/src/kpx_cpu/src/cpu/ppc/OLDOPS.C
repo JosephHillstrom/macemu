c53e294cbde1a826aa63f4f48765b735f3fa03e6
JustinCB <JustinCB@opmbx.org>
2018-05-07 15:05:09 -0400

    started wrapping functions

--- SheepShaver/src/kpx_cpu/src/cpu/ppc/OLDOPS.C
+++ SheepShaver/src/kpx_cpu/src/cpu/ppc/OLDOPS.C
@@ -1,27 +1,37 @@
-#include "cpu/ppc/ppc_wrap.h"
+#include "cpu/ppc/OLDOPS.H"
+#define MAKE_RD(op) ((op & 0x03E00000) >> 21)
+#define MAKE_RA(op) ((op & 0x001F0000) >> 16)
 void bcctr(regpointer regs, uint32 op)
 {
-      uint32 rd = 
-			if (op & 1)
-				*(regs.lr) = *(regs.pc);
-			if (!(rd & 4)) {
-				ctr--;
-				if (rd & 2) {
-					if (ctr)
-						goto bctr_nobranch;
-				} else {
-					if (!ctr)
-						goto bctr_nobranch;
-				}
+	uint32 rd = MAKE_RD(op);
+	uint32 ra = MAKE_RA(op);
+	if (op & 1) {
+		*(regs.lr) = *(regs.pc);
+	}
+	if (!(rd & 4)) {
+		*(regs.ctr)--;
+		if (rd & 2) {
+			if (*(regs.ctr)) {
+				return;
 			}
-			if (!(rd & 0x10)) {
-				if (rd & 8) {
-					if (!(cr & (0x80000000 >> ra)))
-						goto bctr_nobranch;
-				} else {
-					if (cr & (0x80000000 >> ra))
-						goto bctr_nobranch;
-				}
+		} 
+		else {
+			if (!(*(regs.ctr))) {
+				return;
 			}
-pc = ctr & 0xfffffffc;
+		}
+	}
+	if (!(rd & 0x10)) {
+		if (rd & 8) {
+			if (!((*(regs.cr)) & (0x80000000 >> ra))) {
+				return;
+			}
+		} 
+		else {
+			if ((*(regs.cr)) & (0x80000000 >> ra)) {
+				return;
+			}
+		}
+	}
+	*(regs.pc) = ((*(regs.ctr)) & 0xfffffffc);
 }
--- SheepShaver/src/kpx_cpu/src/cpu/ppc/ppc_wrap.h
+++ SheepShaver/src/kpx_cpu/src/cpu/ppc/ppc_wrap.h
@@ -2,26 +2,15 @@
 #define PPC_CPU_WRAP_H
 #include "cpu/ppc/ppc-registers.hpp"
 #ifdef __cplusplus
-#include "ppc-cpu.hpp"
-#define wrap_op_func(func) ##func##_cpp(powerpc_cpu cpu, uint32 op) {func(make_regptr(_regs.regs), op);}
+#include "cpu/ppc/ppc-cpu.hpp"
+#define wrap_op_func(func) static inline void ##func##_cpp(powerpc_cpu cpu, uint32 op) {func(c_registers, op);}
 #define call_op_func(func, cpu, op) ##func##_cpp(cpu, op)
-static inline regpointer make_regptr(struct powerpc_registers regs)
-{
-  regpointer ret;
-  ret.gpr = &(regs.gpr[0]);
-  ret.fpr = &(regs.fpr[0]);
-  ret.vr = &(regs.vr[0]);
-  ret.cr = &(regs.cr.cr);
-  ret.xer.so = &(regs.xer.so);
-  ret.xer.ov = &(regs.xer.ov);
-  ret.xer.ca = &(regs.xer.ca);
-  ret.xer.byte_count = &(regs.xer.byte_count);
-  ret.vscr = &(regs.vscr.vscr);
-  ret.vrsave = &(regs.vrsave);
-  ret.fpscr = &(regs.fpscr);
-  ret.lr = &(regs.lr);
-  ret.ctr = &(regs.ctr);
-  ret.pc = &(regs.pc);
-  return ret;
+extern "C" {
+#endif
+#include "cpu/ppc/OLDOPS.H"
+extern regpointer c_registers;
+#ifdef __cplusplus
 }
+wrap_op_func(bcctr);
+#endif
 #endif
